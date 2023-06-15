59d7fb407b750f5321c151268f1ec4ee2ea0b576
codewithsaz <codewithsaz@gmail.com>
2023-06-15 22:13:46 +0530

    Assosications
    Learnt sequelize associations

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -25,15 +30,41 @@ const res = require("express/lib/response");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
+
     // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Max", email: "test@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
