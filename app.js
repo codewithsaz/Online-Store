d5671a05be318421c5a3453341e58873c336ed59
codewithsaz <codewithsaz@gmail.com>
2023-09-27 10:57:05 +0530

    Established a Mongo-DB connection

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -18,28 +14,18 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const res = require("express/lib/response");
-
-// db.execute("SELECT * FROM products")
-//   .then((results) => {
-//     console.log(results[0]);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
 });
 
 app.use("/admin", adminRoutes);
@@ -47,27 +33,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-
-    // console.log(result);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Max", email: "test@gmail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    // console.log(user);
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect(() => {
+  app.listen(3000);
+});
