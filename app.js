9a1a4ce00ef4174b11fc547ef870efa36e4c8ce8
codewithsaz <codewithsaz@gmail.com>
2023-09-29 11:56:57 +0530

    Added Delete Item from Cart

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findUserbyID("6513c782df15915971f69eab")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
