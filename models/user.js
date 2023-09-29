1bb6c8af67f7accd9873a343092384ab9d6f5190
codewithsaz <codewithsaz@gmail.com>
2023-09-29 12:19:35 +0530

    Added Placing Orders

--- models/user.js
+++ models/user.js
@@ -64,6 +64,7 @@ class User {
         });
       });
   }
+
   deleteItemFromCart(productId) {
     const updatedCartItems = this.cart.items.filter((item) => {
       return item.productId.toString() !== productId.toString();
@@ -77,12 +78,45 @@ class User {
       );
   }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
+  }
+
   static findUserbyID(userId) {
     const db = getDb();
     return db
       .collection("users")
       .findOne({ _id: new ObjectId(userId) })
       .then((user) => {
+        console.log(user);
         return user;
       })
       .catch((err) => {
